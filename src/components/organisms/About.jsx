cd7461567ddd9effbb637b290bcbccc63187308b
Karla Agraz <karla.agraz@gmail.com>
2023-03-09 12:19:52 -0500

    ADD: projects

--- src/components/organisms/About.jsx
+++ src/components/organisms/About.jsx
@@ -1,33 +1,15 @@
-import aboutImage from '../../assets/images/4.jpg'
 import { Educacion } from './Education'
 import { Tools } from './Tools'
 import { Skills } from './Skills'
 import { WorkExperience } from './WorkExperience'
 import { Languages } from '../molecules/Languages'
+import { AboutMe } from './AboutMe'
 
 export function About() {
     return (
         <section className="wrapper mx-auto flex flex-col items-center" id="about">
             <h2 className="title text-4xl mb-4">Acerca de mi</h2>
-            <p className="mb-12 text-xl text-center">La compañera de trabajo más creativa 💻</p>
-            <div className="grid sm:grid-cols-2 gap-8 mb-12">
-                <div>
-                    <img
-                        src={aboutImage}
-                        alt="About img"
-                        className="about-img"
-                    />
-                </div>
-                <div className="flex flex-col gap-4 text-lg">
-                    <p>Soy UX Engineer, me apasiona el diseño y el desarrollo de interfaces, cuento con 2 años de experiencia profesional.
-                    </p>
-                    <p>Mi objetivo es crear soluciones que mejoren la experiencia del usuario tanto en su atractivo, funcionalidad,
-                        simplicidad y facilidad de uso.
-                    </p>
-                    <p>Me gusta estar en constante aprendizaje, compartir conocimientos y jugar videojuegos en mi tiempo libre.</p>
-                    <p>Soy originaria de Venezuela y vivo hace casi 5 años en Perú ☀️</p>
-                </div>
-            </div>
+            <AboutMe />
             <div className="grid md:grid-cols-2 gap-8">
                 <article>
                     <h3 className="title text-3xl mb-4">Experiencia</h3>
--- src/components/organisms/Work.jsx
+++ src/components/organisms/Work.jsx
@@ -1,4 +1,5 @@
-import image from '../../assets/images/placeholder.jpg'
+import coffee from '../../assets/images/projects/coffee.jpg'
+import travels from '../../assets/images/projects/travels.jpg'
 
 export function Work() {
     return (
@@ -6,29 +7,37 @@ export function Work() {
             <h2 className="title text-4xl mb-4">Mi trabajo</h2>
             <p className="mb-12 text-xl text-center">Una selección de los mejores proyectos que he realizado</p>
             <div className="grid sm:grid-cols-2 gap-8">
-                <a href="https://csswoman.github.io/cafe-bistro/" target={"_blank"}>
-                    <article>
+                <a
+                    href="https://csswoman.github.io/cafe-bistro/"
+                    target={"_blank"}
+                    className="p-4 rounded-xl bg-transparent hover:bg-white hover:bg-opacity-10"
+                >
+                    <article className="">
                         <img
-                            src={image}
+                            src={coffee}
                             alt="Café Bistro project"
-                            className='rounded-2xl work-img mb-4'
+                            className="rounded-xl mb-4 aspect-[16/9]"
                         />
-                        <p className='font-bold text-lg'>Café Bistro</p>
+                        <p className="font-bold text-lg">Café Bistro</p>
                         <p>Creado con HTML & CSS</p>
                     </article>
                 </a>
-                <a href="https://csswoman.github.io/travel-and-tours/" target={"_blank"}>
+                <a
+                    href="https://csswoman.github.io/travel-and-tours/"
+                    target={"_blank"}
+                    className="p-4 rounded-xl bg-transparent hover:bg-white hover:bg-opacity-10"
+                >
                     <article>
                         <img
-                            src={image}
+                            src={travels}
                             alt="Travels and Tours project"
-                            className='rounded-2xl work-img mb-4'
+                            className="rounded-xl mb-4 aspect-[16/9]"
                         />
-                        <p className='font-bold text-lg'>Travels and Tours</p>
+                        <p className="font-bold text-lg">Travels and Tours</p>
                         <p>Creado con HTML & CSS</p>
                     </article>
                 </a>
-            </div>
-        </section>
+            </div >
+        </section >
     )
 }
